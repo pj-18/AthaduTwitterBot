e8e6a59fcbbc930a46127fe460e80d9ae3b08f04
PJ\praveen.ponduru <praveenponduru144@gmail.com>
2024-08-25 18:49:00 +0530

    updated few methods

--- tweet.py
+++ tweet.py
@@ -2,26 +2,28 @@ import tweepy
 import os
 from datetime import date
 
-consumer_key = os.environ('API_KEY')
-consumer_secret = os.environ('API_KEY_SECRET')
-access_token = os.environ('ACCESS_TOKEN')
-access_token_secret = os.environ('ACCESS_TOKEN_SECRET')
+bearer_token = os.getenv('BEARER_TOKEN')
+api_key = os.getenv('API_KEY')
+api_key_secret = os.getenv('API_KEY_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
-client = tweepy.Client(consumer_key=consumer_key,
-                    consumer_secret=consumer_secret,
-                    access_token=access_token,
-                    access_token_secret=access_token_secret)
+client = tweepy.Client( bearer_token = bearer_token,
+                    consumer_key = api_key,
+                    consumer_secret = api_key_secret,
+                    access_token = access_token,
+                    access_token_secret = access_token_secret)
 
 # Function to send the tweet
 def tweet_countdown():
     today = date.today()
-
+    release_date = date(2025, 8, 9)
     # Only tweet if today is before the release date
     if today <= release_date:
         days_left = (release_date - today).days
         tweet = "JAI BABU"
         #tweet = f"{days_left} days left until the release!"
-        api.update_status(status=tweet)
+        client.create_tweet(text=tweet)
         print("Tweeted:", tweet)
     else:
         print("No more tweets. The release date has passed.")
